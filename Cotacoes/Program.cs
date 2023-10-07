b62a2524d3491d0391a97b25b7cc564e76846785
Matheus Silva <matheus.car.ms@gmail.com>
2023-10-07 06:56:36 -0300

    feat: conexão com a api do google sheets feita

---
+++ Cotacoes/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+
+namespace Cotacoes{
+    class Program{
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static readonly string ApplicationName = "Dot Tutorials";
+        static readonly string sheet = "pagina";
+        static readonly string SpreadsheetId = "1uypMIKinfs1VS8p2U6y-zzQqqnrPfPkc6V-WTOc1CyI";
+        static SheetsService service;
+
+        static void Main(string[] args){
+            Init();
+            ReadSheet();
+        }
+
+        static void Init(){
+            GoogleCredential credential;
+            //Reading Credentials File...
+            using (var stream = new FileStream("../app_client_secret.json", FileMode.Open, FileAccess.Read)){
+                credential = GoogleCredential.FromStream(stream)
+                    .CreateScoped(Scopes);
+            }
+            // Creating Google Sheets API service...
+            service = new SheetsService(new BaseClientService.Initializer(){
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        static void ReadSheet(){
+            // Specifying Column Range for reading...
+            var range = $"{sheet}!A:E";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(SpreadsheetId, range);
+            // Ecexuting Read Operation...
+            var response = request.Execute();
+            // Getting all records from Column A to E...
+            IList<IList<object>> values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                foreach (var row in values)
+                {
+                    // Writing Data on Console...
+                    Console.WriteLine("{0} | {1}", row[0], row[1]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+        }   
+    }
+}
+
+
+
